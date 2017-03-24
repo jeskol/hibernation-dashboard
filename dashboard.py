f727e21d21d7e9391d649b437d07939b343dd9ce
Jesse Kolbert <krumble@usian.org>
2017-03-24 16:32:06 -0400

    Restricted to only hibernating instances, can now update tags

--- dashboard.py
+++ dashboard.py
@@ -54,7 +54,7 @@ def before_request():
 @app.route('/ServiceOwner/<ownerfilter>', methods=['GET'])
 def display_list(ownerfilter=None):
     ec2 = boto.ec2.connect_to_region(session['region'])
-    filters = {"tag:InstanceHibernate":"*"}
+    filters = {"tag:InstanceHibernate":"*|*"}
     if ownerfilter:
         filters["tag:ServiceOwner"] = ownerfilter
     instances = _instance_list(ec2, filters)
@@ -85,6 +85,34 @@ def start_instance(instanceid):
         return redirect(url_for('display_instance', instanceid=instanceid))
     except boto.exception.EC2ResponseError, e:
         return render_template('oops.html', error={'message': e})
+
+@app.route('/update/<instanceid>', methods=['POST', 'GET'])
+def tag_instance(instanceid):
+    hibernationTag = ""
+    days = []
+    for i in str(range(0,7)):
+        if request.form.get(i, False) == 'on':
+            days.append(i)
+    hours = [ request.form.get('hibstart'), request.form.get('hibstop') ]
+    if len([x for x in hours if str(x) in str(range(0,24))]):
+        hibernationTag = "{},{}|".format(str(hours[0]), str(hours[1]))
+        hibernationTag = hibernationTag + ','.join(days)
+    else:
+        hibernationTag = '|' + ','.join(days)
+    ec2 = boto.ec2.connect_to_region(session['region'])
+    try:
+        i = ec2.get_only_instances([instanceid])[0]
+        htag = i.tags.get('InstanceHibernate', '')
+    except boto.exception.EC2ResponseError, e:
+        return render_template('oops.html', error={'message': e})
+    hibernation = _parse_hibernation(htag)
+    #Restrict to only currently hibernating instances
+    if hibernation['hibernating'] == 1:
+        i.add_tag('InstanceHibernate', hibernationTag)
+        flash('Hibernation updated for {}'.format(instanceid))
+    else:
+        flash('This tool can only update instances with current hibernation.')
+    return redirect(url_for('display_instance', instanceid=instanceid))
 
 
 if __name__ == '__main__':
