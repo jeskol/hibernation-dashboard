34ee052f6523b7999d8bd75ac185ba7022d0e339
Jesse Kolbert <krumble@usian.org>
2017-04-03 13:57:26 -0400

    Some debugging for openshift

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python2
 
+import os
 import boto.ec2
 import boto.exception
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
@@ -116,4 +117,5 @@ def tag_instance(instanceid):
 
 
 if __name__ == '__main__':
+    print os.environ['AWS_ACCESS_KEY_ID']
     app.run(host='0.0.0.0', port=8080)
